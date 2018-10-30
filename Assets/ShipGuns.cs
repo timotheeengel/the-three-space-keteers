3429268b95bc12e54514d4a1b7e6fecb99e82dfd
Tim <engel.timothee@gmail.com>
2018-10-30 13:43:49 +0100

    Cleaned code
    ^ this

--- Assets/ShipGuns.cs
+++ Assets/ShipGuns.cs
@@ -12,11 +12,22 @@ public class ShipGuns : MonoBehaviour
 	private float _coolDownTimer = 0f;
 	[SerializeField] private GameObject _missile;
 	[SerializeField] private float _missileSpeed = 200f;
+	[SerializeField] private float _missileLifeTime = 3f;
 	private GameObject[] _gunBarrels;
+	private GameObject _missileParent;
+	
 	
 	// Use this for initialization
 	void Start ()
 	{
+		_missileParent = GameObject.Find("Missiles");
+		if (_missileParent == null)
+		{
+			_missileParent = new GameObject();
+			_missileParent.transform.position = Vector3.zero;
+			_missileParent.transform.rotation = Quaternion.identity;
+			_missileParent.name = "Missiles";
+		}
 		_gunBarrels = GameObject.FindGameObjectsWithTag("Barrel");
 	}
 	
@@ -32,12 +43,12 @@ public class ShipGuns : MonoBehaviour
 		{
 			foreach (GameObject barrel in _gunBarrels)
 			{
-				// TODO: Why is there no velocity?!
 				GameObject newMissile = Instantiate(_missile, barrel.transform.position, Quaternion.identity);
-				newMissile.GetComponent<Rigidbody>().velocity = newMissile.transform.forward * _missileSpeed;
-				Debug.Log(newMissile.GetComponent<Rigidbody>().velocity);
+				newMissile.GetComponent<Rigidbody>().velocity = transform.forward * _missileSpeed;
+				newMissile.transform.parent = _missileParent.transform;
+				
+				Destroy(newMissile, _missileLifeTime);
 			}
-
 			_coolDownTimer = _coolDown;
 		}
 	}
