8e25448fb3c591786d4e3dc56d5654277997f209
Tim <engel.timothee@gmail.com>
2018-11-05 08:53:57 +0100

    Tweaked World Space rendering for HUD
    ^ this

---
+++ Assets/AsteroidShardDestruction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidShardDestruction : MonoBehaviour {
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Missile")||
+            other.gameObject.CompareTag("Asteroid") ||
+            other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
